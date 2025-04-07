4555c4fc76363591f1ad31ed91c7c92eefd1df1c
gs0xa19f2 <gs0xa19f2@tutamail.com>
2025-04-07 20:26:42 +0300

    07.04.2025 Second Draft Update

---
+++ SQL for Backend Developers/Medium/Payroll Management Database/query_1.sql
@@ -0,0 +1,21 @@
+CREATE PROCEDURE GetEmployeesByDept(
+    IN department_name VARCHAR(45)
+)
+BEGIN 
+    SELECT 
+        e.first_name,
+        e.last_name,
+        j.title AS job_title
+    FROM 
+        employees e
+    JOIN 
+        departments d ON e.department_id = d.id 
+    JOIN 
+        jobs j ON e.job_id = j.id
+    WHERE
+        d.name = department_name
+    ORDER BY 
+        e.first_name;
+END;
+
+CALL GetEmployeesByDept("Office of Finance");
